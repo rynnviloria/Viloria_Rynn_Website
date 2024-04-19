911659854045b77d0c52bbbcdec75495f51d8d11
rynnviloria <rynn.viloria@gmail.com>
2024-04-19 15:36:52 -0700

    Website Deploy

---
+++ js/index.js
@@ -0,0 +1,40 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.card',{
+    y: 400,
+    opacity: 0,
+    duration: 10,
+    ease: "power4.out",
+    stagger: .15,
+    scrollTrigger: {
+        trigger: '.container-flex',
+        start: "top 80%",
+        scrub: true,
+    }
+});
+
+gsap.from('#image',{
+    xPercent: -100,
+    duration: 10,
+    ease: "power4.out",
+    scrollTrigger: {
+        trigger: '.image-anim',
+        markers: true,
+        scrub: true,
+    }
+});
+
+gsap.from('#text',{
+    xPercent: 100,
+    duration: 10,
+    ease: "power4.out",
+    scrollTrigger: {
+        trigger: '.image-anim',
+        markers: true,
+        scrub: true,
+    }
+});
+
+gsap.from ('.scale--anim',{
+    scale: 400,
+})
